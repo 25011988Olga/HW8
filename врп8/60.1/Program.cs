255d65139d32abd483ae2ed2312965e0cda51638
25011988Olga <olga.moseychuk.88@mail.ru>
2022-11-11 12:39:53 +0300

    выполнила задание60

--- врп8/60.1/Program.cs
+++ врп8/60.1/Program.cs
@@ -5,16 +5,16 @@
 // построчно выводить массив, добавляя индексы каждого элемента.
 
 Console.Clear();
-Console.WriteLine($"\nВведите размер массива m x n x p");
-Console.WriteLine("Введите m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите n: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите p: ");
-int p = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"\nВведите размер массива x * y * z");
+Console.WriteLine("Введите x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z: ");
+int z = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($" ");
 
-int[,,] array3D = new int[m, n, p];
+int[,,] array3D = new int[x, y, z];
 InputArray(array3D);
 ReleaseArray(array3D);
 
@@ -24,10 +24,10 @@ void ReleaseArray (int[,,] array3D)
   {
     for (int j = 0; j < array3D.GetLength(1); j++)
     {
-      Console.Write($"m({i}) n({j}) ");
+      Console.Write($"x({i}) y({j}) ");
       for (int k = 0; k < array3D.GetLength(2); k++)
       {
-        Console.Write( $"p({k})={array3D[i,j,k]}; ");
+        Console.Write( $"z({k})={array3D[i,j,k]}; ");
       }
       Console.WriteLine();
     }
@@ -58,13 +58,13 @@ void InputArray(int[,,] array3D)
     }
   }
   int count = 0; 
-  for (int m = 0; m < array3D.GetLength(0); m++)
+  for (int x = 0; x < array3D.GetLength(0); x++)
   {
-    for (int n = 0; n < array3D.GetLength(1); n++)
+    for (int y = 0; y < array3D.GetLength(1); y++)
     {
-      for (int p = 0; p < array3D.GetLength(2); p++)
+      for (int z = 0; z < array3D.GetLength(2); z++)
       {
-        array3D[m, n, p] = temp[count];
+        array3D[x, y, z] = temp[count];
         count++;
       }
     }
